172fdeb47a3868b59b8e0e6adbceb173f63341c4
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-05-01 14:23:11 +0300

    503-508 proje hazırlanmıştır.Onaylar mısın

---
+++ src/Pages/US_503_508_POM.java
@@ -0,0 +1,60 @@
+package Pages;
+
+
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class US_503_508_POM {
+    public US_503_508_POM() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+
+    @FindBy(xpath = "//a[@class='tn-atom js-click-zero-stat']")
+    public WebElement ApplyNow;
+
+    @FindBy(xpath = "//input[@aria-label='name']")
+    public WebElement Fullname;
+
+    @FindBy(xpath = "//input[@aria-label='email']")
+    public WebElement Eposta;
+
+    @FindBy(xpath = "//input[@type='tel']")
+    public WebElement Number;
+
+    @FindBy(xpath = "//select[@id='sb-1667664755026']")
+    public WebElement Country;
+
+    @FindBy(xpath = "//select[@id='sb-1663337581601']")
+    public WebElement Sdet;
+
+    @FindBy(xpath = "//select[@id='sb-1670425500925']")
+    public WebElement Other;
+
+    @FindBy(xpath = "//input[@aria-label='oneline']")
+    public WebElement PromoCode;
+
+    @FindBy(xpath = "//div[@class='t-checkbox__indicator']")
+    public WebElement TermsofUse;
+
+    @FindBy(xpath = "//button[@style='padding: 0px 15px; display: block; width: 160px; font-weight: 600; height: 50px; margin-left: auto; margin-right: auto;']")
+    public WebElement Button;
+
+    @FindBy(xpath = "//div[@class='t-form-success-popup__text t-descr t-descr_sm']")
+    public WebElement thankYouMessage;
+
+    @FindBy(xpath = "//span[@class='t-checkbox__labeltext']/a")
+    public WebElement TermsofUse2;
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/Project/US_508_TermsOfUse.java
+++ src/Project/US_508_TermsOfUse.java
@@ -1,4 +1,39 @@
 package Project;
 
-public class US_508_TermsOfUse {
+import Pages.US_503_508_POM;
+import Utility.BaseDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class US_508_TermsOfUse extends BaseDriver{
+    @Test(groups = "Regression")
+    public void test8() throws IOException {
+        US_503_508_POM elements = new US_503_508_POM();
+        driver.get("https://techno.study/");
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.ApplyNow)).click();
+        elements.Fullname.sendKeys("Test Ayşe Yılmaz");
+        elements.Eposta.sendKeys("test_a.yilmaz@gmail.com");
+        elements.Number.sendKeys("1111111111");
+        elements.Country.sendKeys("Andorra");
+        elements.Sdet.sendKeys("Sdet");
+        elements.Other.sendKeys("Other");
+        elements.PromoCode.sendKeys("abc1234");
+        wait.until(ExpectedConditions.elementToBeClickable(elements.TermsofUse2)).click();
+
+        wait.until(ExpectedConditions.urlContains("terms"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("terms"));
+
+        TearDown();
+
+}
 }
